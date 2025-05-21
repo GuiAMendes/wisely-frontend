b5cad4258c25d595287b50c7dead8da3934ba3af
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2025-05-21 11:20:50 -0300

    refactor: adjust border radius

--- src/globals/theme/borderRadius.ts
+++ src/globals/theme/borderRadius.ts
@@ -1,9 +1,9 @@
-import { RadiusWildcard } from 'styled-components'
+import { RadiusWildcard } from "styled-components";
 
 export const RADIUS_WILDCARDS: Record<RadiusWildcard, string> = {
-  card: '10px',
-  input: '5px',
-  button: '5px',
-  modal: '1rem',
-  none: '0'
-}
+  card: "10px",
+  input: "5px",
+  button: "5px",
+  modal: "1rem",
+  none: "0",
+};
