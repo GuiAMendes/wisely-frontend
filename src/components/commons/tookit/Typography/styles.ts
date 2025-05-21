65dd2e8ebd14bb7e020681f7b97d80638dec9ace
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2025-05-21 10:54:18 -0300

    fix: adjust typography

--- src/components/commons/tookit/Typography/styles.ts
+++ src/components/commons/tookit/Typography/styles.ts
@@ -2,8 +2,6 @@ import styled from "styled-components";
 import { TypographyProps } from "./types";
 
 export const Container = styled.div<TypographyProps>`
-  font-family: sans-serif;
-
   color: ${({ color }) => color};
   font-size: ${({ fontSize }) => fontSize};
   font-weight: ${({ fontWeight }) => fontWeight};
