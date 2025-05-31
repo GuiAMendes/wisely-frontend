fb05a2446e207a965e22b983f8c2982b31e9d544
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-05-31 13:42:44 -0300

    feat: create type to journey handlers

---
+++ src/services/journey/types.ts
@@ -0,0 +1,16 @@
+export type TypeOfJourney = "free" | "full";
+
+export interface JourneyProps {
+  id: string;
+  idDirectory: string;
+  journeyName: string;
+  typeOfJourney: TypeOfJourney;
+  createdAt: string;
+  updatedAt: string;
+  isCompleted: boolean;
+  isActive: boolean;
+}
+
+export interface Journey {
+  props: JourneyProps[];
+}
