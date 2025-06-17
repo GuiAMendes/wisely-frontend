0dec337ab4f5416e1d842111044d3bad872f5fda
guilhermeAm <guilherme.andmendes03@gmail.com>
2025-06-17 17:38:55 -0300

    feat: create handler to mark as completed journey

---
+++ src/services/journey/journey.id.complete.patch/index.ts
@@ -0,0 +1,24 @@
+// Service
+import API from "@services/api";
+
+// Utils
+import { getAuthHeaders } from "@utils/getAuthHeaders";
+
+// Types
+import { HttpResponse, CompleteJourneyInput } from "./response";
+
+export async function CompleteJourney({ journeyId }: CompleteJourneyInput) {
+  const url = `/journey/${journeyId}/complete`;
+  const options = getAuthHeaders();
+
+  try {
+    if (!options) {
+      throw new Error("Token is missing or invalid.");
+    }
+
+    const response = await API.patch<HttpResponse>(url, {}, options);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
---
+++ src/services/journey/journey.id.complete.patch/response.ts
@@ -0,0 +1,8 @@
+export interface CompleteJourneyInput {
+  journeyId: string;
+}
+
+export interface HttpResponse {
+  journeyId: string;
+  status: boolean;
+}
