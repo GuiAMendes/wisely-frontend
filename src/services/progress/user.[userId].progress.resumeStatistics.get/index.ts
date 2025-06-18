3c4eaa6726118fc7b6f04a77bc9ac273a061ab7a
guilhermeAm <guilherme.andmendes03@gmail.com>
2025-06-18 14:43:11 -0300

    feat: create handler to get statistics of progress

---
+++ src/services/progress/user.[userId].progress.resumeStatistics.get/index.ts
@@ -0,0 +1,24 @@
+// Service
+import API from "@services/api";
+
+// Utils
+import { getAuthHeaders } from "@utils/getAuthHeaders";
+
+// Types
+import type { ResumeStatisticsOfProgressInput } from "./response";
+import type { ResumeStatistics } from "../types";
+
+export async function resumeStatistics({ userId }: ResumeStatisticsOfProgressInput) {
+  const url = `/user/${userId}/progress/resumeStatistics`;
+  const options = getAuthHeaders();
+
+  try {
+    if (!options) {
+      throw new Error("Token is missing or invalid.");
+    }
+    const response = await API.get<ResumeStatistics>(url, options);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
