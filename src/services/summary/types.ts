91109cd3dba85cc6c694ad349463306eeabf0b67
guilhermeAm <guilherme.andmendes03@gmail.com>
2025-06-18 16:49:45 -0300

    feat: create typing to summary

---
+++ src/services/summary/types.ts
@@ -0,0 +1,22 @@
+export interface NoteProps {
+  content: string;
+}
+
+export interface Note {
+  props: NoteProps;
+}
+
+export interface SummaryProps {
+  id: string;
+  idTopic: string;
+  title: string;
+  note: Note;
+  createdAt: string;
+  updatedAt: string;
+  completedAt: string;
+  isActive: boolean;
+}
+
+export interface Summary {
+  props: SummaryProps;
+}
