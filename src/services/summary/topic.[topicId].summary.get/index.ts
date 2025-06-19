821c3286d81b0ba578591715f040f225438187f0
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-06-19 00:06:00 -0300

    fix: adjust function to find summary information

--- src/services/summary/topic.[topicId].summary.get/index.ts
+++ src/services/summary/topic.[topicId].summary.get/index.ts
@@ -7,11 +7,7 @@ import { getAuthHeaders } from "@utils/getAuthHeaders";
 // Types
 import type { FindSummaryInput, HttpResponse } from "./response";
 
-export async function findSummary({
-  idTopic,
-  title,
-  noteContent,
-}: FindSummaryInput) {
+export async function findSummary({ idTopic }: FindSummaryInput) {
   const url = `/topic/${idTopic}/summary`;
   const options = getAuthHeaders();
 
@@ -20,11 +16,7 @@ export async function findSummary({
       throw new Error("Token is missing or invalid.");
     }
 
-    const response = await API.post<HttpResponse>(
-      url,
-      { title, noteContent },
-      options
-    );
+    const response = await API.get<HttpResponse>(url, options);
     return response.data.summary;
   } catch (error) {
     console.log(error);
