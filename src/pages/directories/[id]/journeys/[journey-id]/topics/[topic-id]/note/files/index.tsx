28ae507f9df8de4c8979b706199004905d05a602
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2025-06-19 03:45:35 -0300

    feat: creat file path

---
+++ src/pages/directories/[id]/journeys/[journey-id]/topics/[topic-id]/note/files/index.tsx
@@ -0,0 +1,9 @@
+// External libraries
+import React from "react";
+import { FlashCardList } from "@pages/FlashcardList";
+
+const FilesPage: React.FC = () => {
+  return <FlashCardList />;
+};
+
+export default FilesPage;
