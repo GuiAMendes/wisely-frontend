d43fb29c15d9c8437de78879066e5d33f98b5a74
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2025-06-19 01:01:41 -0300

    feat: add flashcard list hook

--- src/components/pages/FlashcardList/hooks/useFlashcardList/index.ts
+++ src/components/pages/FlashcardList/hooks/useFlashcardList/index.ts
@@ -1,10 +1,39 @@
-// Types
-import { UseFlashcardListParams } from './types'
+// External library
+import useSWR from "swr";
+import { toast } from "sonner";
+import { useRouter } from "next/router";
 
-export function useFlashcardList({}: UseFlashcardListParams) {
-  // States
+// Service
+import { listAllFlashcards } from "@services/flashcard";
 
-  // Functions
+export function useFlashCard() {
+  // Hooks
+  const { query } = useRouter();
 
-  return {}
+  const idTopic = query["topic-id"] as string;
+
+  const { data, isLoading, mutate } = useSWR(
+    `/topic/${idTopic}/flashcard`,
+    fetchFlashCard,
+    {
+      revalidateOnFocus: false,
+    }
+  );
+
+  async function fetchFlashCard() {
+    try {
+      if (!idTopic) return;
+
+      return listAllFlashcards({ idTopic });
+    } catch (error) {
+      console.log(error);
+      toast.error("Erro para listar flashCards");
+    }
+  }
+
+  return {
+    flashCardList: data,
+    isLoading,
+    mutate,
+  };
 }
