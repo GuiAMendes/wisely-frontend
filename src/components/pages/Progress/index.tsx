e1ffb597387255936227e7b5fd4da4df50647472
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2025-06-19 05:36:22 -0300

    feat: add firula

--- src/components/pages/Progress/index.tsx
+++ src/components/pages/Progress/index.tsx
@@ -7,11 +7,44 @@ import React from "react";
 import { useProgress } from "./hooks/useProgress";
 
 // Styles
-import { Container } from "./styles";
+import {
+  Card,
+  CardImage,
+  Column,
+  Container,
+  Firula,
+  PageContent,
+} from "./styles";
+import { Navigation } from "@components/structure/Navigation";
+import Image from "next/image";
 
 export const Progress: React.FC = () => {
   // Hooks
   const {} = useProgress();
 
-  return <Container>{/* Code */}</Container>;
+  return (
+    <Container>
+      <Navigation />
+      <PageContent>
+        <></>
+
+        <Firula>
+          <Column>
+            <CardImage>
+              <Image
+                src="/images/progress.png"
+                alt="progress"
+                fill
+                style={{ objectFit: "contain", pointerEvents: "none" }}
+              />
+            </CardImage>
+
+            <Card />
+          </Column>
+
+          <Card />
+        </Firula>
+      </PageContent>
+    </Container>
+  );
 };
--- src/components/pages/Progress/styles.ts
+++ src/components/pages/Progress/styles.ts
@@ -1,3 +1,52 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+export const Container = styled.div`
+  width: 100%;
+  flex: 1;
 
-export const Container = styled.div``
+  display: flex;
+  flex-direction: column;
+`;
+
+export const PageContent = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 40rem;
+
+  gap: 2rem;
+
+  padding: 2rem;
+`;
+
+export const Firula = styled.div`
+  display: flex;
+
+  width: 100%;
+
+  gap: 1rem;
+
+  height: calc(100vh - 10rem);
+
+  align-items: center;
+`;
+
+export const Column = styled.div`
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const CardImage = styled.div`
+  width: 13.5rem;
+  height: 48%;
+
+  position: relative;
+`;
+
+export const Card = styled.div`
+  width: 13.5rem;
+  height: 48%;
+
+  background-color: ${({ theme }) => theme.colors.primary};
+
+  border-radius: 30px;
+`;
